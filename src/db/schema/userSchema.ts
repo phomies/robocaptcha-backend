cbc3e9d398c6d63966603f8b4b5eec54f91a42eb
Daryl Wong <youcanfinddaryl@gmail.com>
2022-02-17 00:11:43 +0800

    added verification level for user

--- src/db/schema/userSchema.ts
+++ src/db/schema/userSchema.ts
@@ -8,6 +8,7 @@ export const userSchema = new Schema({
     dateJoined: { type: Date, default: Date.now() },
     whitelist: [Number],
     blacklist: [Number],
+    verificationLevel: {type: Number, min: 0, max: 3},
     callHistory: [{type: Types.ObjectId, ref: 'Call'}],
     notificationHistory: [{type: Types.ObjectId, ref: 'Notification'}],
 });
