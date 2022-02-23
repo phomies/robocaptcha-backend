5db550d1312dc3138be4dc781a98153258f9f212
Daryl Wong <youcanfinddaryl@gmail.com>
2022-02-23 17:13:27 +0800

    updated phoneNumbers to String

--- src/db/schema/callSchema.ts
+++ src/db/schema/callSchema.ts
@@ -3,7 +3,8 @@ import { Schema, Types } from 'mongoose';
 export const callSchema = new Schema({
     id: Types.ObjectId,
     date: { type: Date, default: Date.now() },
-    from: Number,
+    callSid: String,
+    from: String,
     toUserId: {type: Types.ObjectId, ref: 'User'},
     action: String,
 });
