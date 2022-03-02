7ca14413082dd622be92aa1f811e9a4f5bb1a7d2
Jerald <jeraldssw@gmail.com>
2022-03-02 16:50:08 +0800

    feat: migrate to basic apollo federal server

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,69 @@
-import express from 'express';
 import 'dotenv/config';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
-import indexRouter from './routes/index';
-import { typeDefs } from './data/schema';
-import { resolvers } from './data/resolver';
-import { ApolloServer } from 'apollo-server-express';
-import { dateScalar } from './data/scalars';
+import { initFirebase } from './auth';
+import { ApolloGateway, IntrospectAndCompose } from '@apollo/gateway';
+import { ApolloServer } from 'apollo-server';
+import { PORTS } from './utils/ports';
+import waitOn from 'wait-on';
 
-Object.assign(resolvers, { Date: dateScalar });
+const getUrl = (port: number) => {
+    const BASE_URL = process.env.URL || 'http://localhost:';
 
-const startApolloServer = async () => {
-    const port = process.env.PORT || 3000;
-    const app = express();
-    const server = new ApolloServer({
-        typeDefs,
-        resolvers,
-        formatError: (err: any) => {
-            // TODO - FORMAT ERROR BEFORE SENDING BACK TO CLIENT
-            return err;
-        }
-    });
-
-    await server.start();
-    server.applyMiddleware({ app });
-
-    app.use(logger('dev'));
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: false }));
-    app.use(cookieParser());
-
-    app.use('/', indexRouter);
-
-    app.listen(port, () => {
-        console.log(`App is listening on port ${server.graphqlPath}`);
-    });
+    return BASE_URL + port;
 };
 
-startApolloServer();
+const gateway = new ApolloGateway({
+    supergraphSdl: new IntrospectAndCompose({
+        subgraphs: [{ name: 'users', url: getUrl(PORTS['USERS']) }],
+    }),
+});
+
+const server = new ApolloServer({
+    gateway: gateway,
+});
+
+initFirebase();
+
+const options = {
+    resources: Object.values(PORTS).map((port) => `tcp:${port}`),
+};
+
+waitOn(options).then(() => {
+    server.listen(PORTS['ROOT']).then(({ url }) => {
+        console.log(`Server is listening at ${url}`);
+    });
+});
+
+// const startApolloServer = async () => {
+//     initFirebase();
+
+//     const port = process.env.PORT || 3000;
+//     const app = express();
+//     app.use(logger('dev'));
+//     app.use(express.json());
+//     app.use(express.urlencoded({ extended: false }));
+//     app.use(cookieParser());
+//     // app.use(authMiddleware);
+//     app.use('/', indexRouter);
+
+//     const server = new ApolloServer({
+//         schema: buildFederatedSchema([{ typeDefs, resolvers }]),
+//         context: ({ req }) => {
+//             req;
+//         },
+//         formatError: (err: any) => {
+//             // TODO - FORMAT ERROR BEFORE SENDING BACK TO CLIENT
+//             return err;
+//         },
+//     });
+
+//     await server.start();
+//     server.applyMiddleware({
+//         app,
+//     });
+
+//     app.listen(port, () => {
+//         console.log(`App is listening on port ${server.graphqlPath}`);
+//     });
+// };
+
+// startApolloServer();
