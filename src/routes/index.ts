965eb036f61ac601b3a149a8cfe342b03ee68369
Jerald <jeraldssw@gmail.com>
2022-03-02 18:33:16 +0800

    perf: remove redundant files

--- src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-
-router.get('/', function (req, res) {
-    res.status(200).json({ message: 'CallCAPTCHA backend is working' });
-});
-
-export default router;
