5e34261cc7e00613da98b4841b2fe23d859b85da
Jerald <jeraldssw@gmail.com>
2022-03-14 23:54:21 +0800

    feat(contact): add extension query

--- src/services/contact/contact.resolvers.ts
+++ src/services/contact/contact.resolvers.ts
@@ -1,26 +1,32 @@
 import axios from 'axios';
+import { IContext } from "../../common/interface";
 import { Contact } from '../../db';
 import { Connection } from './contact.interface';
 
 const GOOGLE_API_URL = 'https://people.googleapis.com/v1/people/me/connections?personFields=names,phoneNumbers';
 
 export const ContactResolvers = {
-    User: {},
+    User: {
+        contacts: async (user: any) => {
+            const contacts = await Contact.find({ userId: user._id });
+            return contacts;
+        },
+    },
     Query: {
-        syncContacts: async (_: any, __: any, context: any) => {
+        syncContacts: async (_: any, __: any, context: IContext) => {
             // First page
-            const { connections, nextPageToken: currentPageToken } = await getGoogleContacts(context.token);
+            const { connections, nextPageToken: currentPageToken } = await getGoogleContacts(context.gapiToken);
 
             // Not all google accounts have contacts
             if (connections) {
-                let bulkContacts = getBulkOperations(connections, []);
+                let bulkContacts = getBulkOperations(connections, [], context.uid);
                 let nextPage = currentPageToken;
 
                 // Middle pages
                 while (nextPage) {
-                    const { connections, nextPageToken } = await getGoogleContacts(context.token, nextPage);
+                    const { connections, nextPageToken } = await getGoogleContacts(context.gapiToken, nextPage);
                     nextPage = nextPageToken;
-                    bulkContacts = getBulkOperations(connections, bulkContacts);
+                    bulkContacts = getBulkOperations(connections, bulkContacts, context.uid);
                 }
                 await Contact.bulkWrite(bulkContacts);
             }
@@ -38,7 +44,7 @@ const getGoogleContacts = async (token: string, pageToken?: string) => {
     return response.data;
 };
 
-const getBulkOperations = (connections: Connection[], result: any[]) => {
+const getBulkOperations = (connections: Connection[], result: any[], id: string) => {
     connections.forEach((connection: Connection) => {
         if (connection.names && connection.phoneNumbers) {
             // Retrieve contact name that is set as primary by owner
@@ -49,7 +55,7 @@ const getBulkOperations = (connections: Connection[], result: any[]) => {
                     updateOne: {
                         // Users may change number or name for certain contacts
                         filter: { $or: [{ number: phone.canonicalForm }, { name: name }] },
-                        update: { $set: { number: phone.canonicalForm, name: name } },
+                        update: { $set: { number: phone.canonicalForm, name: name, userId: id } },
                         upsert: true,
                     },
                 });
--- src/services/contact/contact.typeDefs.ts
+++ src/services/contact/contact.typeDefs.ts
@@ -4,12 +4,20 @@ export const ContactTypeDefs = gql`
     scalar Date
 
     type Contact {
+        _id: ID
         name: String
-        phoneNumber: String
+        number: String
+        isBlocked: Boolean
+        createdAt: Date
+        updatedAt: Date
+    }
+
+    extend type User @key(fields: "_id") {
+        _id: String @external
+        contacts: [Contact]
     }
 
     extend type Query {
-        getContacts: [Contact]
         syncContacts: Boolean
     }
 `;
